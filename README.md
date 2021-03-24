bc7f49e8d464b3ae39f2601116ffb12babbe380e
Eduardo Martinez <eduardo31794@gmail.com>
2021-03-23 22:11:01 -0600

    Updating Readme and script

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Password-Generator
+
+## Description
+This project was a homework dessigned for the ITESM Bootcamp, in wich we were supossed to be familiarize a little better with Javascript and to play with it. I had a lot of problems while doing it, because I just could not undertand the logic of the language. In fact it`s incomplete but I'm studiyng really hard and pretty soon I'm going to be able to complete it. 
+
+## Installation
+This project has been deployed to GitHub. To get this project up and running, you can follow the deployment link.
+
+https://github.com/Eduadomtz/Password-Generator
+
+## Usage
+
+The project offers an interface created with HTML, CSS and Javascript. 
+
+The first view is presents a button that will send you a series of prompts and confirrms. 
+
+<img src="./assets/images/01.png">
+
+
+When you press the button "Generate Password" a prompt will appear asking you the length of your password.
+
+<img src="./assets/images/02.png">
+
+If you type a value smaller than 8 or bigger than 128 an other prompt will appear asking you to type a valid value (between 8 and 128).
+
+<img src="./assets/images/03.png">
+
+Then they will appear 4 confirm windows gathering data to create you random password.
+
+<img src="./assets/images/04.png">
+
+Finally it was supossed to appear the password, I just need to keep searching and I will update the repository.
+
+
+## Features
+HTML Pages
+Index.html
+
+CSS Page
+Styles.css
+
+Contains media queries
+Javascript Page
--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var characters = ['abcdefghijklmnñopqrstuvwyz','ABCEDFGHIJKLMNÑOPQRSTUVWYZ','0123456789', '!#$%&()*+,-./:;<=>?@[\]^_{|}'];
+var user = true;
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 // Write password to the #password input
 function writePassword() {
@@ -13,26 +18,46 @@ function writePassword() {
 
 function generatePassword() {
   var userChoice = 0;
-
   userChoice = window.prompt("How many characters do you want in your password?");
-
   while (userChoice < 8 || userChoice > 128) {
     userChoice = window.prompt("Your password must have a length of at least 8 characters and be less than 128");
-  } 
+    };
 
-  while (userChoice = true) {
-    var specialCha = window.confirm("Click ok to confirm for including special characters");
-    var numeric = window.confirm("Click ok to confirm for including numeric characters");
-    var lower = window.confirm("Click ok to confirm for including lowercase characters");
-    var upper = window.confirm("Click ok to confirm for including uppercase characters");
-    if (specialCha == false && numeric == false && lower == false && upper == false){
-      userChoice = true;
-    } else {
-      userChoice= false;
-    }
-  }
+     while (user = true) { 
+      var specialCha = window.confirm("Click ok to confirm for including special characters");
+      var numeric = window.confirm("Click ok to confirm for including numeric characters");
+      var lower = window.confirm("Click ok to confirm for including lowercase characters");
+      var upper = window.confirm("Click ok to confirm for including uppercase characters");
+      if (specialCha == false && numeric == false && lower == false && upper == false){
+        user = true;
+      } else {
+        user= false;
+      }
+      break;
+    } 
 
-  return password; 
+      if (character) {
+        password = characters [Math.floor[Math.random ()* characters.length]]
+        console.log(characters)
+      };
+    
+      if (numeric) {
+        password = numbers [Math.floor[Math.random ()* numbers.length]]
+        console.log(numbers)
+      };
+    
+      if (lower) {
+        password = lowercase [Math.floor[Math.random ()* lowercase.length]]
+        console.log(lowercase)
+      };
+    
+      if (upper) { 
+        password = uppercase [Math.floor[Math.random ()* uppercase.length]]
+        console.log(uppercase)
+      };
+    
+    passwordText.value = password;
   }
   
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
