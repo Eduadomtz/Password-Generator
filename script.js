d5f8380779d9b3820b326083e87b0f9cfe556ef0
Eduardo Martinez <eduardo31794@gmail.com>
2021-04-03 13:32:09 -0600

    Updating functional script

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -8,7 +7,73 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
+
 // Write password to the #password input
+
+
+function generatePassword() {
+  
+  var userChoice = window.prompt("Your password must have a length of at least 8 characters and be less than 128");
+  if (userChoice < 8 || userChoice > 128) {
+    generatePassword();
+  }
+
+  var unite = "";
+  var character = window.confirm("Click ok to confirm including special characters");
+  var numeric = window.confirm("Click ok to confirm including numeric characters");
+  var lower = window.confirm("Click ok to confirm including lowercase characters");
+  var upper = window.confirm("Click ok to confirm including uppercase characters");
+
+  if (character === false && numeric === false && lower === false && upper === false) {
+    alert("Your password must include at least one element");
+    generatePassword();
+  } else if (character === true && numeric === true && lower === true && upper === true) {
+    unite = unite.concat(characters);
+    unite = unite.concat(numbers);
+    unite = unite.concat(lowercase);
+    unite = unite.concat(uppercase);
+  } else if (character === true && numeric === false && lower === false && upper === false) {
+    unite = unite.concat(characters);
+  }else if (character === true && numeric === true && lower === false && upper === false) {
+    unite = unite.concat(characters);
+    unite = unite.concat(numbers);
+  }else if (character === true && numeric === true && lower === true && upper === false) {
+    unite = unite.concat(characters);
+    unite = unite.concat(numbers);
+    unite = unite.concat(lowercase);
+  } else if (character === true && numeric === false && lower === true && upper === false) {
+    unite = unite.concat(characters);
+    unite = unite.concat(lowercase);
+  }else if (character === true && numeric === false && lower === false && upper === true) {
+    unite = unite.concat(characters);
+    unite = unite.concat(uppercase);
+  } else if (character === false && numeric === true && lower === true && upper === true) {
+    unite = unite.concat(numbers);
+    unite = unite.concat(lowercase);
+    unite = unite.concat(uppercase);
+  } else if (character === false && numeric === false && lower === true && upper === true) {
+    unite = unite.concat(lowercase);
+    unite = unite.concat(uppercase);
+  } else if (character === false && numeric === false && lower === false && upper === true) {
+    unite = unite.concat(uppercase);
+  } else if (character === false && numeric === true && lower === false && upper === true) {
+    unite = unite.concat(numbers);
+    unite = unite.concat(uppercase);
+  } else if (character === false && numeric === true && lower === false && upper === false) {
+    unite = unite.concat(numbers);
+  } else if (character === false && numeric === false && lower === true && upper === false) {
+    unite = unite.concat(lowercase);
+  }
+
+  console.log(unite);
+
+  var passwordResult = "";
+  for (var i = 0; i < userChoice; i++) {
+    passwordResult += unite.charAt(Math.floor(Math.random() * unite.length));
+  };
+  return passwordResult;
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -16,48 +81,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function generatePassword() {
-  var userChoice = 0;
-  userChoice = window.prompt("How many characters do you want in your password?");
-  while (userChoice < 8 || userChoice > 128) {
-    userChoice = window.prompt("Your password must have a length of at least 8 characters and be less than 128");
-    };
-
-     while (user = true) { 
-      var specialCha = window.confirm("Click ok to confirm for including special characters");
-      var numeric = window.confirm("Click ok to confirm for including numeric characters");
-      var lower = window.confirm("Click ok to confirm for including lowercase characters");
-      var upper = window.confirm("Click ok to confirm for including uppercase characters");
-      if (specialCha == false && numeric == false && lower == false && upper == false){
-        user = true;
-      } else {
-        user= false;
-      }
-      break;
-    } 
-
-      if (character) {
-        password = characters [Math.floor[Math.random ()* characters.length]]
-        console.log(characters)
-      };
-    
-      if (numeric) {
-        password = numbers [Math.floor[Math.random ()* numbers.length]]
-        console.log(numbers)
-      };
-    
-      if (lower) {
-        password = lowercase [Math.floor[Math.random ()* lowercase.length]]
-        console.log(lowercase)
-      };
-    
-      if (upper) { 
-        password = uppercase [Math.floor[Math.random ()* uppercase.length]]
-        console.log(uppercase)
-      };
-    
-    passwordText.value = password;
-  }
-  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
